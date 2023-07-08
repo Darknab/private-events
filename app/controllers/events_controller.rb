658c2e3b4bfbcd7adc7cecfadefee4c7006c22ae
Darknab <darknab@live.fr>
2023-07-08 18:02:53 +0100

    Add the category column to events Add the new method to the events controller Create a view for creating a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,20 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+
+  def new
+    @user = User.find(current_user[:id])
+    @event = @user.created_events.new
+  end
+
+  def create
+    @user = User.find(current_user[:id])
+    @event = @user.created_events.create(event_params)
+    redirect_to user_path(@user)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :description, :date, :category)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_173304) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_08_163305) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_173304) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "creator_id", null: false
+    t.string "category"
     t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
