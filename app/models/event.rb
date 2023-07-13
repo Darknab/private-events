5b58e9f4f613c31fcb8987b221a2c39cc4ea553b
Darknab <darknab@live.fr>
2023-07-13 11:17:46 +0100

    Add validations to the user and event models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,19 @@
 class Event < ApplicationRecord
-    belongs_to :creator, class_name: "User"
-    has_many :participations, foreign_key: "attended_event_id"
-    has_many :attendees, class_name: "User", through: :participations, source: :attendee
-    scope :upcoming, -> {where("date > ?", DateTime.now)}
-    scope :past, -> {where("date < ?", DateTime.now)}
+  belongs_to :creator, class_name: "User"
+  has_many :participations, foreign_key: "attended_event_id"
+  has_many :attendees, class_name: "User", through: :participations, source: :attendee
+
+  scope :upcoming, -> {where("date > ?", DateTime.now)}
+  scope :past, -> {where("date < ?", DateTime.now)}
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :date, presence: true, :date_cannot_be_in_the_past
+  validates :category, inclusion: {in: %w(Public Private), "%{value} is not valid"}
+
+  def date_cannot_be_in_the_past
+    if date.present? && date < DateTime.now()
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates name:, presence: true, uniqueness: true
+  validates adress:, presence: true
+
 end
